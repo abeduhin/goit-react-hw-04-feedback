b170b79974a31732e9856c53ad618957cf7367cd
Oleksii Biedukhin <abeduhin17@gmail.com>
2023-01-02 23:59:47 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+// Вказуємо початковий стан
+  handleFeedback = e => {
+    if (e === 'Good') {
+      this.setState({ good: this.state.good + 1 });
+    } else if (e === 'Neutral') {
+      this.setState({ neutral: this.state.neutral + 1 });
+    } else if (e === 'Bad') {
+      this.setState({ bad: this.state.bad + 1 });
+    }
+  };
+
+  // Змінюємо стан через this.setState ({}) та прописуємо умови - якщо e (SyntheticEvent) дорівнює GOOD то лічільник good збільшується на 1, якщо інше - то інше +1
+
+  countTotalFeedback = () => {
+    let total = this.state.good + this.state.neutral + this.state.bad;
+    return total;
+  };
+  // Прописуємо сумуючу функцію 
+  countPositiveFeedbackPercentage = () => {
+    if (this.countTotalFeedback() === 0) {
+      return 0;
+    }
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+  // Рахуємо тільки позитивні відгуки та ділемо на загальні
+  render() {
+    return (
+      <div>
+      
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['Good', 'Neutral', 'Bad']}
+            onLeaveFeedback={this.handleFeedback}
+          />
+        </Section>
+        {/*Робимо розмітку секції с заголовком та кнопками  */}
+
+        <Section title="Statistics">
+          {this.countTotalFeedback() !== 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback"></Notification>
+          )}
+        </Section>
+        {/* Робимо розмітку секції статистики з заголовком та умовою якщо відгуків не має то рендер message якщо є то рендер секціі з варіантами */}
+      </div>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,22 @@
+import propTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div className={css.feedbackBtnList}>
+    {options.map((option, index) => (
+      <button
+        key={index}
+        onClick={() => onLeaveFeedback(option)}
+        className={css.feedbackBtn}
+      >
+        {/* Події onClick передаємо обробника події функцію onLeaveFeedback з параметром option*/}
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+FeedbackOptions.propTypes = {
+  options: propTypes.arrayOf(propTypes.string).isRequired,
+  onLeaveFeedback: propTypes.func.isRequired,
+};
